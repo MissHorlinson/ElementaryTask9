51c6db9e30d5d2a68257ce06eff499147aa8a81d
Maria <mary24018@gmail.com>
2019-10-28 19:03:41 +0300

    Initial commit

--- src/main/java/util/Output.java
+++ src/main/java/util/Output.java
@@ -3,9 +3,7 @@ package util;
 import java.util.ArrayList;
 
 public class Output {
-    public void startInfo(String value) {
-        System.out.println("Enter the " + value);
-    }
+    public void startInfo(String value) { System.out.println("Enter the " + value); }
 
     public void negativeNumber() {
         System.out.println("It should be positive number > 0");
@@ -16,7 +14,7 @@ public class Output {
         pow = number;
         int i = 1;
         for (Integer value : powList) {
-            System.out.println(i++ + ") " + number + " * " + pow + " = " + value);
+            System.out.println(i++ + ". " + number + " * " + pow + " = " + value);
             number = value;
         }
     }
